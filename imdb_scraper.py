671079bc22cce535563cf0b1ef2ca503034ab509
7014gree <7014gree@gmail.com>
2022-11-07 18:54:49 +0000

    Reads html and outputs to text.txt

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -3,4 +3,7 @@ from bs4 import BeautifulSoup
 
 r = requests.get("https://www.imdb.com/title/tt0110912/?ref_=nv_sr_srsg_0")
 html_string = r.text
-print(html_string)
+soup = BeautifulSoup(html_string, 'html.parser')
+
+with open('text.txt', 'w', encoding="utf-8") as f:
+        f.write(soup.prettify())
